6052ae036cd2ca4113721a9f711efe22a385b4a1
James Ortiz <jamesaortiz75@gmail.com>
2020-07-08 23:42:56 -0400

    First Demo Project useing ASP.NET

---
+++ FirstProject/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FirstProject.Models;
+using System;
+
+namespace FirstProject.Controllers
+{
+    public class HomeController : Controller
+    {
+
+       
+
+        //Returns a basic View:
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            string viewModel = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView", viewModel);
+        }
+       
+
+
+        /*
+        public string Index()
+        {
+            return "Hello World!";
+        }
+        */
+        /*
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        */
+    }
+}
